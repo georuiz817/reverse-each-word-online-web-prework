139f2eebade9a228319251d8f83b6b35e6597784
George Ruiz <georuiz817@gmail.com>
2019-05-28 22:12:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(sentence1)
-  sentence1.split()
+sentence1 =  "Hello there, and how are you?"
+ sentence1.split(",") 
+ sentence1.each do |x| 
+  puts x.reverse 
+ end
+ end
   
